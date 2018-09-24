8aa9dcc72c7676a106ab69c73e2706e6626ed32d
JulesGalbraith <julig9@gmail.com>
2018-09-24 15:20:02 -0400

    replaced enemy and avatar with mueller and trump

--- exercises/exercise2/exercise2/js/script.js
+++ exercises/exercise2/exercise2/js/script.js
@@ -43,7 +43,9 @@ var tallyFont;
 //preloads the text and images necessary to bling out game
 function preload() {
 tallyFont = loadFont("assets/fonts/Grad.TTF");
-flowerFont =loadFont("assets/fonts/Floralia.ttf")
+flowerFont =loadFont("assets/fonts/Floralia.ttf");
+avatar = loadImage("assets/images/theDonald.png");
+enemy =loadImage("assets/images/mueller.png;")
 }
 // setup()
 //
@@ -114,6 +116,10 @@ function draw() {
   // Update the enemy's position based on its velocity
   enemyX = enemyX + enemyVX;
 
+//assigns a value to avatar and enemy size
+
+avatarSize =(avatar.width*0.1);
+enemySize =(enemy.width*0.1)
   // Check if the enemy and avatar overlap - if they do the player loses
   // We do this by checking if the distance between the centre of the enemy
   // and the centre of the avatar is less that their combined radii
@@ -157,7 +163,7 @@ function draw() {
     enemyY = random(0,height);
     // Increase the enemy's speed and size to make the game harder
     enemySpeed = enemySpeed + enemySpeedIncrease;
-    enemySize = enemySize + enemySizeIncrease;
+    enemySize = enemySize * enemySizeIncrease;
 }
   // display dodge tally
   textSize(tallySize);
@@ -174,14 +180,10 @@ function draw() {
   // Display the current number of successful dodges in the console
   console.log(dodges);
 
-  // The player is black
-  fill(0);
-  // Draw the player as a circle
-  ellipse(avatarX,avatarY,avatarSize,avatarSize);
+  // The player is DONALD TRUMP
+  image(avatar,avatarX,avatarY,avatarSize);
 
-  // The enemy is red
-  fill(255,0,0);
-  // Draw the enemy as a circle
-  ellipse(enemyX,enemyY,enemySize,enemySize);
+  // The enemy is ROBERT MUELLER
+  image(enemy,enemyX,enemyY,enemySize)
 
 }
