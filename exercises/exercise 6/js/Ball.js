c356efaf62b0f9c9a3bceea0f241f43198eba151
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2018-10-30 22:43:55 -0400

    E6: gave ball a fill value in ball.display

--- exercises/exercise 6/js/Ball.js
+++ exercises/exercise 6/js/Ball.js
@@ -56,6 +56,8 @@ Ball.prototype.isOffScreen = function () {
 //
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
+  //////////////fixed/////////////////
+ fill(255);
   rect(this.x,this.y,this.size);
 }
 
